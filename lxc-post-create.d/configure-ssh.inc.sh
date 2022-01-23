06b5429d67b510d0f213455658953ba4c738febb
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2022-01-23 17:28:13 +0100

    Allow for skipping the authorized keys setup

--- lxc-post-create.d/configure-ssh.inc.sh
+++ lxc-post-create.d/configure-ssh.inc.sh
@@ -3,6 +3,10 @@
 # prevent sshd's stupid behaviour of overriding the locale environment:
 sed -i 's,^AcceptEnv,#AcceptEnv,' "$TGT_ROOT"/etc/ssh/sshd_config
 
-# configure ssh-access for the root account
-mkdir -pv "$TGT_ROOT"/root/.ssh
-cp -v "$AUTH_KEYS" "$TGT_ROOT"/root/.ssh/authorized_keys
+if [ -z "$AUTH_KEYS" ]; then
+    echo "*** WARNING ***: var '\$AUTH_KEYS' not set, skipping authorized_keys setup!"
+else
+    # configure ssh-access for the root account
+    mkdir -pv "$TGT_ROOT"/root/.ssh
+    cp -v "$AUTH_KEYS" "$TGT_ROOT"/root/.ssh/authorized_keys
+fi
