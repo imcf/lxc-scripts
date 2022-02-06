abdcb92918dfeeed45941f8c293d353ccc500063
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2022-02-06 18:09:53 +0100

    Clear sources list before adding entries (avoid duplicates)

--- lxc-post-create.d/configure-sources-list-ubuntu-all.inc.sh
+++ lxc-post-create.d/configure-sources-list-ubuntu-all.inc.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # set up sources.list for APT
+TGT_SOURCESLIST="$TGT_ROOT"/etc/apt/sources.list
+echo > "$TGT_SOURCESLIST"
 
 for CUR_SUITE in "$SUITE $SUITE-updates $SUITE-security" ; do
-    echo "deb $MIRROR $CUR_SUITE main universe multiverse" >> "$TGT_ROOT"/etc/apt/sources.list
+    echo "deb $MIRROR $CUR_SUITE main universe multiverse" >> "$TGT_SOURCESLIST"
 done
