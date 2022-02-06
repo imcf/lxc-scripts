de0c9777ccd7134483e16e701e259b881b5b369d
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2022-02-06 18:14:51 +0100

    Add missing "restricted" component

--- lxc-post-create.d/configure-sources-list-ubuntu-all.inc.sh
+++ lxc-post-create.d/configure-sources-list-ubuntu-all.inc.sh
@@ -5,5 +5,5 @@ TGT_SOURCESLIST="$TGT_ROOT"/etc/apt/sources.list
 echo > "$TGT_SOURCESLIST"
 
 for CUR_SUITE in "$SUITE $SUITE-updates $SUITE-security" ; do
-    echo "deb $MIRROR $CUR_SUITE main universe multiverse" >> "$TGT_SOURCESLIST"
+    echo "deb $MIRROR $CUR_SUITE main restricted universe multiverse" >> "$TGT_SOURCESLIST"
 done
